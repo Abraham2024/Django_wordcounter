e84fbd9eb619321c5e2323078029c00f37fcaf73
Muhammed Ibrahim Amanokhai <ibrahimjunior08@gmail.com>
2023-10-29 16:11:20 +0100

    Update urls.py

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,7 @@
-django.urls import path
+from django.urls import path
 from . import views
 
 urlpatterns = [
   path("", views.index, name = "index"),
-  path("counter", views.counter, name = "counter")
-  ]
+  path("counter", views.counter, name = "counter") 
+ ]
