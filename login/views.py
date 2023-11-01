6b851cef552896bc6cf213ae92a007bc968abaea
Abraham2024 <ibrahimjunior08@gmail.com>
2023-11-01 19:30:55 +0100

    my_commit

--- login/views.py
+++ login/views.py
@@ -7,6 +7,17 @@ def index(request):
     feature1.id = 0
     feature1.name = 'Fast'
     feature1.details = 'Our services are very fast'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Reliable'
+    feature2.details = 'Our services are very reliable'
+
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Easy to use'
+    feature3.details = 'Our services are very easy to use'
     #context = {
            # 'name':  'Abraham',
            # 'age' : 23,
@@ -16,7 +27,7 @@ def index(request):
          #include the dictionary name when using dictionary and the var key and value when using a variable
 
 
-    return render(request, 'index.html', {'feature': feature1})
+    return render(request, 'index.html', {'feature1': feature1, 'feature2' : feature2, 'feature3' : feature3})
 
 
 def counter(request):
