620f1e058ea0c0fc6f4b7e98042f50e7d2c2058f
Abraham2024 <ibrahimjunior08@gmail.com>
2023-11-01 18:37:12 +0100

    my_commit

--- login/views.py
+++ login/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 # Create your views here.
 def index(request):
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our services are very fast'
     #context = {
            # 'name':  'Abraham',
            # 'age' : 23,
@@ -11,7 +16,7 @@ def index(request):
          #include the dictionary name when using dictionary and the var key and value when using a variable
 
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'Feature': feature})
 
 
 def counter(request):
