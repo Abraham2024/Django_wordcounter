3f5dddbc018e7b9817825643d6d6b3b2cfbf7ae9
Abraham2024 <ibrahimjunior08@gmail.com>
2023-11-05 13:26:17 +0100

    my_commit

--- scholarpad/settings.py
+++ scholarpad/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-92(d*%=0qmsomhvdt!))-xt8c(lj0uj(lw5j3vu9q0o6mplb8f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/' 
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS =[ os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
  
 
